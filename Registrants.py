299ea3d948d08bd0d53e312f789a37fcdd23a45c
Sarah R Collins <jgold885@gmail.com>
2020-07-27 13:50:36 -0400

    reduced unnecessary instance variables for Registrants

--- Registrants.py
+++ Registrants.py
@@ -2,11 +2,8 @@ import json
 
 class Registrants:
     
-    def __init__(self, in_name, in_email, in_phone):
-        self._name = in_name
-        self._email = in_email
-        self._phone = in_phone
-        self._info = {"name": self._name, "email": self._email, "phone": self._phone}
+    def __init__(self, name, email, phone):
+        self._info = {"name": name, "email": email, "phone": phone}
         self._registrant = {"registrant":self._info}
         self._json = json.dumps(self._registrant)
    
